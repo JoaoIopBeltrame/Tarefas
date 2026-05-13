800a959964e2c860abc540086ed54582db63e9c0
João Iop Beltrame <115894856+JoaoIopBeltrame@users.noreply.github.com>
2026-05-13 17:15:58 -0300

    Delete Tarefas/6-7ª - Semana - Listas & Vetores/Vetores directory

--- Tarefas/6-7ª - Semana - Listas & Vetores/Vetores/Quadrado-dos-numeros-vetor.py
+++
@@ -1,9 +0,0 @@
-vetor = [0] * 10
-for i in range(10):
-    vetor[i] = int(input(f'Digite o {i + 1}° numero\n> '))
-quadrado = [v ** 2 for v in vetor]
-print(quadrado)
-print(vetor)    
-    
-
-
--- Tarefas/6-7ª - Semana - Listas & Vetores/Vetores/Vetor-soma.py
+++
@@ -1,7 +0,0 @@
-A = [1, 0, 5, -2, -5, 7]
-print(A)
-soma = A[0] + A[1] + A[5]
-print(f'{A[0]} + {A[1]} + {A[5]} = {soma}')
-A[4] = 100
-for i in A:
-    print(i, end=' ')
